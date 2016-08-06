c398cfaab314604e7604c8df8f84f489478bc0fc
mutaihillary <mutaihillary@yahoo.com>
2016-08-06 14:46:58 +0300

    upload

---
+++ sum.py
@@ -0,0 +1,10 @@
+#Create a function that gets the sum of all the digits in a list given as a parameter
+#pseudo code
+#define a function
+#get the values
+#add
+#print the sum
+
+numbers = [1, 2, 3]
+numsum = (sum(numbers))
+print(numsum)
