cb0cb9f4e665130401f51fab5fe1d6ae98a7e08e
mutaihillary <mutaihillary@yahoo.com>
2016-11-13 22:14:37 +0300

    bank account snippet

--- bank_account.py
+++ bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount():
-    def __init__(self, balance):
+    def __init__(self, balance=500):
         self.balance = balance
 
     def deposit(self, cash):
@@ -7,8 +7,33 @@ class BankAccount():
 
     def withdraw(self, cash):
         if cash > self.balance:
-            return "invalid transaction"
+            return "Cannot withdraw beyond the minimum account balance"
         self.balance -= cash
 
 class MinimumBalanceAccount(BankAccount):
     pass
+
+class Customer (object):
+"""A customer of ABC Bank with a checking account. Customers have the
+following properties:
+Attributes:
+name: A string representing the customer's name.
+balance: A float tracking the current balance of the customer's account.
+"""
+def __init__(self, name, balance=0.0):
+"""Return a Customer object whose name is *name* and starting
+balance is *balance*."""
+self.name = name
+self.balance = balance
+def withdraw(self, amount):
+"""Return the balance remaining after withdrawing *amount*
+dollars."""
+if amount > self.balance:
+raise RuntimeError ('Amount greater than available balance.')
+self.balance -= amount
+return self.balance
+def deposit(self, amount):
+"""Return the balance remaining after depositing *amount*
+dollars."""
+self.balance += amount
+return self.balance
